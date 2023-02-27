532271d1d67c83890da22bf67dc1d37e9ff26b3b
Lagutina Elizaveta <below8585@mail.ru>
2023-02-27 10:52:55 +0300

    Задачи с лекционного занятия

---
+++ 001/Program.cs
@@ -0,0 +1,47 @@
+﻿// Примеры матриц из лекции //
+
+// Пример с текстом //
+
+string[,] table = new string[2, 5];
+// // table[0,0]   table[0,1]  table[0,2]  table[0,3]  table[0,4]
+// // table[1,0]   table[1,1]  table[1,2]  table[1,3]  table[1,4]
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int colums = 0; colums < 5; colums++)
+    {
+        Console.Write($" -{table[rows, colums]}- ");
+    }
+    Console.WriteLine();
+}
+
+// Пример с цифрами //
+int[,] matrix = new int[3, 4];
+// Метод печати матрицы
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// Метод заполнения матрицы
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+// Вызов методов
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вычисление факториала
+double Factorial(int n)
+{
+    if (n == 1) return 1;  // так как 1!=1
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
